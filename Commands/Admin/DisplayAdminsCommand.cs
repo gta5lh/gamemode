9588d40ae7b544f702e434d4aa1e29d5c67c1c1a
Maksim Logvinenko <lbyte00@gmail.com>
2021-03-08 20:38:04 +0200

    Implement /admins admin command

---
+++ Commands/Admin/DisplayAdminsCommand.cs
@@ -0,0 +1,18 @@
+﻿using Gamemode.Models.Admin;
+using Gamemode.Models.Player;
+using GTANetworkAPI;
+
+namespace Gamemode.Commands.Admin
+{
+    class DisplayAdminsCommand : Script
+    {
+        private const string DisplayAdminsCommandUsage = "Использование: /admins";
+
+        [Command("admins", DisplayAdminsCommandUsage, Alias = "a", SensitiveInfo = true, GreedyArg = true, Hide = true)]
+        [AdminMiddleware(AdminRank.Junior)]
+        public void DisplayAdmins(CustomPlayer admin)
+        {
+            admin.SendChatMessage($"Админы онлайн: {AdminsCache.GetAdminNames()}");
+        }
+    }
+}
