c38853da3b9dc7bdcd3241cbbc3f49ad0bf271b2
Maksim Logvinenko <lbyte00@gmail.com>
2021-03-08 20:33:00 +0200

    Add IsAdmin util

--- Models/Admin/AdminRank.cs
+++ Models/Admin/AdminRank.cs
@@ -10,14 +10,14 @@
 
     public static class AdminRankMethods
     {
-        public static bool AtLeastJunior(this AdminRank adminRank)
-        {
-            return (ushort)adminRank >= 1;
-        }
-
         public static bool AtLeast(this AdminRank adminRank, AdminRank atLeast)
         {
             return adminRank >= atLeast;
+        }
+
+        public static bool IsAdmin(this AdminRank adminRank)
+        {
+            return adminRank >= AdminRank.Junior;
         }
     }
 }
