824a22b91c61a509a214a934d343bbf0c2f1ce31
Maksim Logvinenko <lbyte00@gmail.com>
2021-03-10 21:33:20 +0200

    Implement teleport and getcar

---
+++ Commands/Admin/TeleportCommand.cs
@@ -0,0 +1,118 @@
+﻿// <copyright file="Teleport.cs" company="lbyte00">
+// Copyright (c) lbyte00. All rights reserved.
+// </copyright>
+
+namespace Gamemode
+{
+    using System;
+    using Gamemode.Commands.Admin;
+    using Gamemode.Models.Admin;
+    using Gamemode.Models.Player;
+    using GTANetworkAPI;
+
+    public class TeleportCommand : Script
+    {
+        private const string TeleportCommandUsage = "Использование: /tp {player_id} | /tp {player_id} {player_id}. Пример: [/tp 2], [/tp 2 10]";
+
+        [Command("teleport", TeleportCommandUsage, Alias = "tp", SensitiveInfo = true, GreedyArg = true, Hide = true)]
+        [AdminMiddleware(AdminRank.Junior)]
+        public void Teleport(CustomPlayer admin, string firstPlayerIdInput = null, string secondPlayerIdInput = null)
+        {
+            if (firstPlayerIdInput == null)
+            {
+                admin.SendChatMessage(TeleportCommandUsage);
+                return;
+            }
+
+            ushort firstPlayerId;
+            ushort secondPlayerId;
+
+            try
+            {
+                firstPlayerId = ushort.Parse(firstPlayerIdInput);
+            }
+            catch (Exception)
+            {
+                admin.SendChatMessage(TeleportCommandUsage);
+                return;
+            }
+
+            if (secondPlayerIdInput != null)
+            {
+                try
+                {
+                    secondPlayerId = ushort.Parse(secondPlayerIdInput);
+                }
+                catch (Exception)
+                {
+                    admin.SendChatMessage(TeleportCommandUsage);
+                    return;
+                }
+
+                CustomPlayer firstPlayer = PlayerUtil.GetById(firstPlayerId);
+                if (firstPlayer == null)
+                {
+                    admin.SendChatMessage($"Пользователь с DID {firstPlayerId} не найден");
+                    return;
+                }
+
+                CustomPlayer secondPlayer = PlayerUtil.GetById(secondPlayerId);
+                if (secondPlayer == null)
+                {
+                    admin.SendChatMessage($"Пользователь с DID {secondPlayerId} не найден");
+                    return;
+                }
+
+                firstPlayer.Position = secondPlayer.Position;
+                admin.SendChatMessage($"Вы телепортировали {firstPlayer.Name} [{firstPlayer.Id}] к {secondPlayer.Name} [{secondPlayer.Id}]");
+                return;
+            }
+
+            CustomPlayer targetPlayer = PlayerUtil.GetById(firstPlayerId);
+            if (targetPlayer == null)
+            {
+                admin.SendChatMessage($"Пользователь с DID {firstPlayerId} не найден");
+                return;
+            }
+
+            admin.Position = targetPlayer.Position;
+            admin.SendChatMessage($"Вы телепортировались к {targetPlayer.Name} [{targetPlayer.Id}]");
+        }
+
+        private const string GetCarCommandUsage = "Использование: /getcar {id}. Пример: /getcar 0";
+
+        [Command("getcar", GetCarCommandUsage, Alias = "gc", SensitiveInfo = true, GreedyArg = true, Hide = true)]
+        [AdminMiddleware(AdminRank.Junior)]
+        public void GetCar(CustomPlayer admin, string vehicleIdInput = null)
+        {
+            if (vehicleIdInput == null)
+            {
+                admin.SendChatMessage(GetCarCommandUsage);
+                return;
+            }
+
+            ushort vehicleId;
+
+            try
+            {
+                vehicleId = ushort.Parse(vehicleIdInput);
+            }
+            catch (Exception)
+            {
+                admin.SendChatMessage(GetCarCommandUsage);
+                return;
+            }
+
+            Vehicle vehicle = VehicleUtil.GetById(vehicleId);
+            if (vehicle == null)
+            {
+                admin.SendChatMessage($"Автомобиль с ID {vehicleIdInput} отсутствует");
+                return;
+            }
+
+            vehicle.Position = admin.Position;
+            vehicle.Rotation = admin.Rotation;
+            admin.SendChatMessage($"Вы телепортировали автомобиль {vehicleIdInput} к себе");
+        }
+    }
+}
