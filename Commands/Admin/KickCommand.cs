d1848fca6ba1a8c61815fef7ae3e9a2e51b345c5
Maksim Logvinenko <lbyte00@gmail.com>
2021-03-11 23:12:28 +0200

    Implement kick

---
+++ Commands/Admin/KickCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Gamemode.Models.Admin;
+using Gamemode.Models.Player;
+using Gamemode.Utils;
+using GTANetworkAPI;
+
+namespace Gamemode.Commands.Admin
+{
+    public class KickCommand : BaseCommandHandler
+    {
+        private const string KickCommandUsage = "Использование: /kick {player_id} {причина}. Пример: /ban 1 Бот";
+
+        [Command("kick", KickCommandUsage, SensitiveInfo = true, Hide = true)]
+        [AdminMiddleware(AdminRank.Junior)]
+        public void Ban(CustomPlayer admin, string targetIdInput = null, string reason = null)
+        {
+            if (targetIdInput == null || reason == null)
+            {
+                admin.SendChatMessage(KickCommandUsage);
+                return;
+            }
+
+            ushort targetId;
+
+            try
+            {
+                targetId = ushort.Parse(targetIdInput);
+            }
+            catch (Exception)
+            {
+                admin.SendChatMessage(KickCommandUsage);
+                return;
+            }
+
+            CustomPlayer targetPlayer = PlayerUtil.GetById(targetId);
+            if (targetPlayer == null)
+            {
+                admin.SendChatMessage($"Пользователь с DID {targetId} не найден");
+                return;
+            }
+
+            targetPlayer.Kick();
+            Chat.SendColorizedChatMessageToAll(ChatColor.AdminAnnouncementColor, $"Администратор: {admin.Name} кикнул {targetPlayer.Name}. Причина: {reason}");
+            this.Logger.Warn($"Administrator {admin.Name} kicked {targetPlayer.Name}");
+        }
+    }
+}
