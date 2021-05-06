b16d997917c87975baa585c5f50d2edfb0e1ad74
Maksim Logvinenko <lbyte00@gmail.com>
2021-05-06 19:05:06 +0300

    Improve position command to check for vehicle

---
+++ Commands/Admin/PositionCommand.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="Position.cs" company="lbyte00">
+// Copyright (c) lbyte00. All rights reserved.
+// </copyright>
+
+namespace Gamemode
+{
+    using Gamemode.Commands.Admin;
+    using Gamemode.Models.Admin;
+    using Gamemode.Models.Player;
+    using GTANetworkAPI;
+
+    public class PositionCommand : Script
+    {
+        [Command("position", "Usage: /position {player_id}", Alias = "pos", SensitiveInfo = true, GreedyArg = true)]
+        [AdminMiddleware(AdminRank.Junior)]
+        public void Position(CustomPlayer admin, string playerID)
+        {
+            if (admin.IsInVehicle)
+            {
+                admin.SendChatMessage($"X: {admin.Vehicle.Position.X}, Y: {admin.Vehicle.Position.Y}, Z: {admin.Vehicle.Position.Z}, Heading: {admin.Vehicle.Heading}");
+            }
+            else
+            {
+                admin.SendChatMessage($"X: {admin.Position.X}, Y: {admin.Position.Y}, Z: {admin.Position.Z}, Heading: {admin.Heading}");
+            }
+        }
+    }
+}
