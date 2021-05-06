7e19970bd70e374ff7e9d28095f02e4a976d236a
Maksim Logvinenko <lbyte00@gmail.com>
2021-05-06 20:18:48 +0300

    Add skins for gang NPCs

--- Models/Gangs/Ballas.cs
+++ Models/Gangs/Ballas.cs
@@ -22,7 +22,7 @@ namespace Gamemode
             this.CarMarker = new Marker(new Vector3(84.7, -1972.8, 20.84), this.GangColor, (MarkerType)36, "Автомобиль", new CarSelectionEvent());
             this.CarSpawn = new Spawn(new Vector3(88.7, -1967.8, 20.75), -39.1f);
             this.ItemMarker = new Marker(new Vector3(102.9, -1959.3, 20.8), this.GangColor, (MarkerType)41, "Снаряжение", new ItemSelectionEvent());
-            this.Npc = new Npc(new Vector3(86.7, -1950, 20.8), -54, "Старший", PedHash.Beach01AFM);
+            this.Npc = new Npc(new Vector3(86.7, -1950, 20.8), -54, "Старший", PedHash.BallaOrig01GMY);
             this.BlipColor = 50;
         }
     }
