b3b67bccca27daf2e4565afbdebdef369550f2a9
Maksim Logvinenko <lbyte00@gmail.com>
2021-05-08 13:35:19 +0300

    Add test commands

---
+++ Commands/Test/TestCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gamemode.Commands.Admin;
+using Gamemode.Models.Admin;
+using Gamemode.Models.Player;
+using GTANetworkAPI;
+
+namespace Gamemode.Commands.Test
+{
+    public class TestCommand : Script
+    {
+        [Command("tmoney", "/tmoney", Alias = "tm", GreedyArg = true)]
+        [AdminMiddleware(AdminRank.Owner)]
+        public void Money(CustomPlayer player, string message = null)
+        {
+            player.SendChatMessage($"{player.Money}");
+        }
+
+        [Command("tlogin", "/tlogin", Alias = "tl", GreedyArg = true)]
+        [AdminMiddleware(AdminRank.Owner)]
+        public void Login(CustomPlayer player, string message = null)
+        {
+            player.LoggedInAt = DateTime.UtcNow;
+            player.SendChatMessage($"{player.LoggedInAt}");
+        }
+    }
+}
