2b20c3f45ce03c2cb5120dd6b9eb38bb35ade8f2
Maksim Logvinenko <lbyte00@gmail.com>
2021-05-14 18:41:56 +0300

    Add spawn blips

--- Models/Npc/Npc.cs
+++ Models/Npc/Npc.cs
@@ -11,7 +11,23 @@ namespace Gamemode.Models.Npc
         public string Name { get; }
         public PedHash Model { get; }
 
-        public IColShapeEventHandler colShapeEventHandler{ get; }
+        public int? Blip { get; }
+        public string? BlipName { get; }
+
+        public IColShapeEventHandler colShapeEventHandler { get; }
+
+        public Npc(Vector3 position, float heading, string name, PedHash model, IColShapeEventHandler colShapeEventHandler, int blip, string blipName)
+        {
+            if (colShapeEventHandler == null) throw new ArgumentNullException("colshapeEnterEvent");
+
+            this.Position = position;
+            this.Heading = heading;
+            this.Name = name;
+            this.Model = model;
+            this.colShapeEventHandler = colShapeEventHandler;
+            this.Blip = blip;
+            this.BlipName = blipName;
+        }
 
         public Npc(Vector3 position, float heading, string name, PedHash model, IColShapeEventHandler colShapeEventHandler)
         {
@@ -40,6 +56,11 @@ namespace Gamemode.Models.Npc
                 ColShape colshape = NAPI.ColShape.CreateCylinderColShape(this.Position, 2, 1);
                 colshape.OnEntityEnterColShape += this.colShapeEventHandler.OnEntityEnterColShape;
                 colshape.OnEntityExitColShape += this.colShapeEventHandler.OnEntityExitColShape;
+            }
+
+            if (this.Blip != null)
+            {
+                NAPI.Blip.CreateBlip(this.Blip.Value, this.Position, 1, 0, this.BlipName, 255, 0, true);
             }
         }
     }
