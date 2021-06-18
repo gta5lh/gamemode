cd7ed478f4dc18497006a37b1ded69380a378a17
LeonKote <roman892265@gmail.com>
2021-06-18 16:04:18 +0500

    UPD: Голосовой чат

--- Controllers/VoiceChatController.cs
+++ Controllers/VoiceChatController.cs
@@ -9,14 +9,19 @@ namespace Gamemode.Controllers
         [RemoteEvent("add_voice_listener")]
         public void AddVoiceListener(CustomPlayer player, CustomPlayer target)
         {
-            if (target == null || !target.Exists || !ChatService.CheckMute(player)) return;
-
+            if (target == null || !target.Exists) return;
+            if (ChatService.CheckMute(player))
+            {
+                player.TriggerEvent("muted");
+                return;
+            }
             player.EnableVoiceTo(target);
         }
 
         [RemoteEvent("remove_voice_listener")]
         public void RemoveVoiceListener(CustomPlayer player, CustomPlayer target)
         {
+            if (target == null || !target.Exists) return;
             player.DisableVoiceTo(target);
         }
     }
