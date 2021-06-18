f164ba4676d1cf31f69140e84a5be7e62819ebcf
LeonKote <roman892265@gmail.com>
2021-06-18 14:43:33 +0500

    Контроллер голосового чата

---
+++ Controllers/VoiceChatController.cs
@@ -0,0 +1,21 @@
+﻿using Gamemode.Models.Player;
+using GTANetworkAPI;
+
+namespace Gamemode.Controllers
+{
+    public class VoiceChatController : Script
+    {
+        [RemoteEvent("add_voice_listener")]
+        public void AddVoiceListener(CustomPlayer player, CustomPlayer target)
+        {
+            if (player.HasData("mute")) return;
+            player.EnableVoiceTo(target);
+        }
+
+        [RemoteEvent("remove_voice_listener")]
+        public void RemoveVoiceListener(CustomPlayer player, CustomPlayer target)
+        {
+            player.DisableVoiceTo(target);
+        }
+    }
+}
