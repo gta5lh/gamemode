af6c3346a813ca55843fa2ad4257c8a34d20f3c0
LeonKote <roman892265@gmail.com>
2021-06-18 16:06:34 +0500

    UPD: Голосовой чат

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -5,6 +5,7 @@
 namespace Gamemode
 {
     using Gamemode.Models.Player;
+    using Gamemode.Services.Player;
     using GTANetworkAPI;
 
     public class ChatController : Script
@@ -12,7 +13,7 @@ namespace Gamemode
         [ServerEvent(Event.ChatMessage)]
         private void ChatMessage(CustomPlayer player, string message)
         {
-            if (!Services.Player.ChatService.CheckMute(player)) return;
+            if (ChatService.CheckMute(player)) return;
 
             NAPI.Chat.SendChatMessageToAll(string.Format("{0}{1} [{2}]:~s~ {3}", player.ChatColor, player.Name, player.Id, message));
         }
