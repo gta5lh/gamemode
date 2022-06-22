3247427fd4e5430c58065dfd0e2669d295a12dc5
Maksim Logvinenko <lbyte00@gmail.com>
2022-06-22 20:53:43 +0300

    CU-2fz3ma5 Implement notification on deposit funded

---
+++ game-server/client_packages/cs_packages/gamemode/Controllers/DonationController.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="DonationController.cs" company="lbyte00">
+// Copyright (c) lbyte00. All rights reserved.
+// </copyright>
+
+namespace GamemodeClient.Controllers
+{
+	using System.Collections.Generic;
+	using GamemodeCommon.Authentication.Models;
+	using static GamemodeClient.Controllers.Cef.Cef;
+	using Newtonsoft.Json;
+	using RAGE;
+	using RAGE.Ui;
+	using System;
+	using GamemodeClient.Models;
+	using GamemodeCommon.Models;
+
+	public class DonationController : Events.Script
+	{
+		public DonationController()
+		{
+			Events.Add("DisplayDonationNotification", this.OnDisplayDonationNotification);
+		}
+
+		private void OnDisplayDonationNotification(object[] request)
+		{
+			long amount = (long)request[0];
+			DisplayNotification(new Notification(string.Format("Тебе на счет было начислено {0} LC. Спасибо за поддержку нашего проекта!", amount), 0, 10000, NotificationType.Success));
+			RAGE.Game.Audio.PlaySoundFrontend(-1, "CHECKPOINT_PERFECT", "HUD_MINI_GAME_SOUNDSET", true);
+		}
+	}
+}
---
+++ server/Controllers/DonationController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using Gamemode.Models.Gangs;
+using Gamemode.Models.Player;
+using Gamemode.Models.Spawn;
+using GTANetworkAPI;
+using EasyNetQ;
+
+namespace Gamemode.Controllers
+{
+	public class DonationController : Script
+	{
+		[Queue("deposit.made", ExchangeName = "eventbus")]
+		public class MyMessage
+		{
+			public long Amount { get; set; }
+			public long PlayerID { get; set; }
+		}
+
+		[ServerEvent(Event.ResourceStartEx)]
+		private async void ResourceStartEx(string resourceName)
+		{
+			var bus = RabbitHutch.CreateBus("host=localhost");
+
+			await bus.PubSub.SubscribeAsync<MyMessage>(
+				string.Empty, msg =>
+				{
+					NAPI.Task.Run(() =>
+					{
+						CustomPlayer targetPlayer = PlayerUtil.GetByStaticId(msg.PlayerID);
+						if (targetPlayer == null)
+						{
+							return;
+						}
+
+						NAPI.ClientEvent.TriggerClientEvent(targetPlayer, "DisplayDonationNotification", msg.Amount);
+					});
+				}
+			);
+		}
+	}
+}
