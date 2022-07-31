4e5f40b522395f09fd1fd9d8ff59b331d4e24945
Maksim Logvinenko <lbyte00@gmail.com>
2022-07-31 11:04:54 +0300

    CU-2k64567 Move AdminAnnouncement to own file

---
+++ server/Game/Admin/Commands/AdminAnnouncement.cs
@@ -0,0 +1,29 @@
+// <copyright file="AdminChat.cs" company="Lost Heaven">
+// Copyright (c) Lost Heaven. All rights reserved.
+// </copyright>
+
+namespace Gamemode.Game.Admin.Commands
+{
+	using Gamemode.Game.Admin.Models;
+	using Gamemode.Game.Chat;
+	using Gamemode.Game.Player.Models;
+	using GTANetworkAPI;
+
+	public class AdminAnnouncement : BaseHandler
+	{
+		private const string AdminAnnouncementUsage = "Использование: /adminannouncement {сообщение}. Пример: /aa Уважаемые игроки...";
+
+		[Command("adminannouncement", AdminAnnouncementUsage, Alias = "aa", SensitiveInfo = true, GreedyArg = true, Hide = true)]
+		[AdminMiddleware(AdminRank.Junior)]
+		public static void OnAdminAnnouncement(CPlayer admin, string? message = null)
+		{
+			if (message == null)
+			{
+				admin.SendChatMessage(AdminAnnouncementUsage);
+				return;
+			}
+
+			NAPI.Chat.SendChatMessageToAll($"{ChatColor.AdminAnnouncementColor}Администратор {admin.Name}: {message}");
+		}
+	}
+}
--- server/Game/Admin/Commands/AdminChat.cs
+++ server/Game/Admin/Commands/AdminChat.cs
@@ -12,7 +12,6 @@ namespace Gamemode.Game.Admin.Commands
 	public class AdminChat : BaseHandler
 	{
 		private const string AdminChatUsage = "Использование: /adminchat {сообщение}. Пример: /ac Привет коллеги!";
-		private const string AdminAnnouncementUsage = "Использование: /adminannouncement {сообщение}. Пример: /aa Уважаемые игроки...";
 
 		[Command("adminchat", AdminChatUsage, Alias = "ac", SensitiveInfo = true, GreedyArg = true, Hide = true)]
 		[AdminMiddleware(AdminRank.Junior)]
@@ -25,19 +24,6 @@ namespace Gamemode.Game.Admin.Commands
 			}
 
 			Cache.SendMessageToAllAdminsChat($"{admin.Name}: {message}");
-		}
-
-		[Command("adminannouncement", AdminAnnouncementUsage, Alias = "aa", SensitiveInfo = true, GreedyArg = true, Hide = true)]
-		[AdminMiddleware(AdminRank.Junior)]
-		public static void OnAdminAnnouncement(CPlayer admin, string? message = null)
-		{
-			if (message == null)
-			{
-				admin.SendChatMessage(AdminAnnouncementUsage);
-				return;
-			}
-
-			NAPI.Chat.SendChatMessageToAll($"{ChatColor.AdminAnnouncementColor}Администратор {admin.Name}: {message}");
 		}
 	}
 }
